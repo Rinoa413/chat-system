33c11778081135f9d80df5504647828eaef236ca
Ayumu0414 <runamana1255133@gmail.com>
2025-05-25 22:28:20 +0900

    桜のアニメーション追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 import PostComment from './components/PostCommet/PostComment';
 import ShowComment from './components/ShowComment/ShowComment';
 
@@ -7,8 +7,28 @@ function App() {
     <>
       <PostComment />
       <ShowComment />
-    </>
-  )
-}
 
-export default App
+      <div className="sakura-container">
+        {Array.from({ length: 25 }).map((_, i) => {
+          const size = 20 + Math.random() * 20;
+          return (
+            <div
+              className="sakura"
+              key={i}
+              style={{
+                left: `${Math.random() * 100}vw`,
+                animationDelay: `${Math.random() * 5}s`,
+                animationDuration: `${5 + Math.random() * 5}s`,
+                width: `${size}px`,
+                height: `${size}px`,
+                zIndex: -1,
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+export default App;
+
